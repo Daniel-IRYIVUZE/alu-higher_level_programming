64c3d3769f2461894acad0ef828e6ea582246df9
Daniel-IRYIVUZE <d.iryivuze@alustudent.com>
2023-11-21 08:07:46 -0800

    Made by Daniel

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""Defines a Rectangle class """
+"""Defines a Rectangle class."""
+
+
 class Rectangle:
     """Represent a rectangle."""
 
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
-
         Args:
             width (int): The width of the new rectangle.
             height (int): The height of the new rectangle.
@@ -41,28 +42,27 @@ class Rectangle:
 
     def area(self):
         """Return the area of the Rectangle."""
-        return self.__width * self.__height
+        return (self.__width * self.__height)
 
     def perimeter(self):
         """Return the perimeter of the Rectangle."""
         if self.__width == 0 or self.__height == 0:
-            return 0
-        return (self.__width * 2) + (self.__height * 2)
+            return (0)
+        return ((self.__width * 2) + (self.__height * 2))
 
     def __str__(self):
         """Return the printable representation of the Rectangle.
-
         Represents the rectangle with the # character.
         """
         if self.__width == 0 or self.__height == 0:
-            return ""
+            return ("")
 
         rect = []
         for i in range(self.__height):
             [rect.append('#') for j in range(self.__width)]
             if i != self.__height - 1:
                 rect.append("\n")
-        return "".join(rect)
+        return ("".join(rect))
 
     def __repr__(self):
         """Return the string representation of the Rectangle."""
