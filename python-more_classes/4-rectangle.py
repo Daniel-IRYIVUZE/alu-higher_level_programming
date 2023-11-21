1d10331a7ad0a80d990f5b1db1943124442de811
Daniel-IRYIVUZE <d.iryivuze@alustudent.com>
2023-11-21 08:02:53 -0800

    Made by Daniel

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -68,4 +68,4 @@ class Rectangle:
         """Return the string representation of the Rectangle."""
         rect = "Rectangle(" + str(self.__width)
         rect += ", " + str(self.__height) + ")"
-        return rect
+        return (rect)
