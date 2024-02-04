430c9342bcb221bc305de5465542c353f721bc18
Daniel IRYIVUZE <139581457+Daniel-IRYIVUZE@users.noreply.github.com>
2024-02-04 16:27:49 +0200

    Made by Daniel

--- javascript-warm_up/100-let_me_const.js
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/node
-global.myVar = 333;
--- javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-const msg = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-let i = 0;
-while (i < msg.length) {
-  console.log(msg[i]);
-  i++;
-}
