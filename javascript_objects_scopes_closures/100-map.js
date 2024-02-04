b063a2cc66853f64e26fa0c2532f70731352d934
Daniel IRYIVUZE <139581457+Daniel-IRYIVUZE@users.noreply.github.com>
2024-02-04 16:38:47 +0200

    Made by Daniel

--- javascript_objects_scopes_closures/100-map.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-const list = require('./100-data').list;
-
-const newList = list.map((element, index) => element * index);
-console.log(list);
-console.log(newList);
--- javascript_objects_scopes_closures/9-logme.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-let callCount = 0;
-
-exports.logMe = function (item) {
-  console.log(`${callCount}: ${item}`);
-  callCount += 1;
-};
