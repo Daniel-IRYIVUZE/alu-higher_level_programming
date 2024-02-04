2f8442052b5bcfe9c5172105ddf8d569f381cc27
Daniel-IRYIVUZE <d.iryivuze@alustudent.com>
2024-02-04 07:16:32 -0800

    Made by Daniel

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -7,4 +7,3 @@ function factorial (n) {
 
 const num = parseInt(process.argv[2]);
 console.log(factorial(num));
-
--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -8,4 +8,3 @@ if (args.length === 0) {
 } else {
   console.log('Arguments found');
 }
-
